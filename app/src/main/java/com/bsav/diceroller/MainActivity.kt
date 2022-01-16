26c70aff1e6b2793a9604ed37f2e177d84c0e646
Brayan Arias <bryan_arias@gap.com>
2022-01-15 19:10:16 -0500

    making the dice to roll dinamically

--- app/src/main/java/com/bsav/diceroller/MainActivity.kt
+++ app/src/main/java/com/bsav/diceroller/MainActivity.kt
@@ -1,11 +1,36 @@
- package com.bsav.diceroller
+package com.bsav.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageDice: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageDice = findViewById(R.id.image_dice)
+        val rollButton: Button = findViewById(R.id.button_roll)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val random = Random.nextInt(6) + 1
+        imageDice.setImageResource(
+            when (random) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+        )
     }
 }
